e2401dcdd0715b6ab55849cd45530f8b1c98900a
Fedenko Maksym <59320919+Fedich09@users.noreply.github.com>
2022-10-24 17:25:03 +0300

    initial commit

---
+++ src/main/java/com/ua/cinema/club/uacinema/UacinemaApplication.java
@@ -0,0 +1,13 @@
+package com.ua.cinema.club.uacinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UacinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UacinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/cinema/club/uacinema/UacinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.cinema.club.uacinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UacinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
